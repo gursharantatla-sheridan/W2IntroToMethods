e02d5505634be6237089164e53b1a5ce0e78a6b0
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-15 17:27:14 -0500

    complete

---
+++ IntroToEnums/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IntroToEnums
+{
+    internal class Program
+    {
+        enum CardValue
+        {
+            Two = 2,
+            Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+        }
+
+        enum Suit
+        {
+            Heart, Diamond, Club, Spade
+        }
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 7; i++)
+            {
+                CardValue cardValue = (CardValue)rnd.Next(2, 15);
+                Suit suit = (Suit)rnd.Next(4);
+
+                Console.WriteLine(cardValue + " of " + suit);
+            }
+        }
+    }
+}
